e5cbb9b3d3b9d3620df38ab81568491f16c32265
Tedro <sktedro@gmail.com>
2021-03-05 23:25:41 +0100

    Messed up .gitignore. All's good now

---
+++ sort/sort.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef struct{
+  int score;
+  char *content;
+} Line;
+
+int getFileLength(FILE *f){
+  int count = 0;
+  char c;
+  while(1){
+    c = fgetc(f);
+    if(c == '\n')
+      count++;
+    if(c == EOF){
+      break;
+    }
+  }
+  fseek(f, 0, SEEK_SET);
+  return count;
+}
+
+bool readLines(Line *line, FILE *f, int lines){
+  int lineLen;
+  for(int l = 0; l < lines; l++){
+    lineLen = 100;
+    line[l].content = malloc(lineLen);
+    if(!line[l].content)
+      return false;
+    line[l].content[0] = '\0';
+
+    for(int i = 0; ; i++){
+      if(i >= lineLen - 2){
+        lineLen += 10;
+        char *p = realloc(line[l].content, lineLen);
+        if(!p)
+          return false;
+        line[l].content = p;
+      }
+      line[l].content[i] = fgetc(f);
+      line[l].content[i+1] = '\0';
+      if(line[l].content[i] == '\n' || line[l].content[i] == EOF)
+        break;
+    }
+  }
+  return true;
+}
+
+void getScores(Line *line, int lines){
+  char *scoreStr = malloc(10);
+  for(int i = 0; i < lines; i++){
+    for(int j = 0; line[i].content[j] != '\n' && line[i].content[j] != EOF; j++){
+      scoreStr[j] = line[i].content[j];
+      if(line[i].content[j] == ';'){
+        line[i].score = strtol(scoreStr, NULL, 10);
+        break;
+      }
+    }
+  }
+  free(scoreStr);
+}
+
+void swap(Line *line, int i1, int i2){
+  char *tmpStr = line[i1].content;
+  int tmpInt = line[i1].score;
+
+  line[i1].content = line[i2].content;
+  line[i1].score = line[i2].score;
+
+  line[i2].content = tmpStr;
+  line[i2].score = tmpInt;
+}
+
+void sort(Line *line, int len){
+  int i, j;
+  for (i = 0; i < len-1; i++)
+    for (j = 0; j < len-i-1; j++)
+      if(line[j].score < line[j+1].score)
+        swap(line, j, j+1);
+}
+
+void printToFile(Line *line, int lines, FILE *f){
+  for(int i = 0; i < lines; i++)
+    fputs(line[i].content, f);
+}
+
+void freeAll(Line *line, int lines){
+  for(int i = 0; i < lines; i++)
+    free(line[i].content);
+  free(line);
+}
+
+int main(int argc, char **argv){
+  if(argc < 2){
+    printf("./sortOut [path to file]\n");
+    printf("eg.: './sortOut ../data/weights001.o'\n");
+    return -1;
+  }
+  //printf("%s\n", argv[1]);
+  FILE *f = fopen(argv[1], "r");
+  if(!f){
+    printf("File not found\n");
+    return -1;
+  }
+  int lines = getFileLength(f);
+  //printf("Lines: %d\n", lines);
+  Line *line = malloc((lines + 1) * sizeof(Line));
+  if(!line)
+    return -1;
+  if(!readLines(line, f, lines))
+    return -1;
+  fclose(f);
+
+  getScores(line, lines);
+  
+  sort(line, lines);
+
+
+  f = fopen(argv[1], "w");
+  if(!f){
+    printf("File not found\n");
+    return -1;
+  }
+
+  printToFile(line, lines, f);
+
+  fclose(f);
+
+  freeAll(line, lines);
+}
---
+++ wgen/wgen.c
@@ -0,0 +1,183 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+
+FILE *f_i;
+FILE *f_o;
+
+float ai_birdSpeedW, ai_birdHeightW, ai_canvasHeightW, ai_distanceToBarW, ai_barHeightW;
+
+char filename_i[] = "./data/weightsXXX_i";
+char filename_o[] = "./data/weightsXXX_o";
+char delim = ';';
+
+void printWeights(char *buffer){
+  snprintf(buffer, 20, "%f;", ai_birdSpeedW);
+  fputs(buffer, f_i);
+  printf("%s", buffer);
+  snprintf(buffer, 20, "%f;", ai_birdHeightW);
+  fputs(buffer, f_i);
+  printf("%s", buffer);
+  snprintf(buffer, 20, "%f;", ai_canvasHeightW);
+  fputs(buffer, f_i);
+  printf("%s", buffer);
+  snprintf(buffer, 20, "%f;", ai_distanceToBarW);
+  fputs(buffer, f_i);
+  printf("%s", buffer);
+  snprintf(buffer, 20, "%f", ai_barHeightW);
+  fputs(buffer, f_i);
+  fputc('\n', f_i);
+  printf("%s\n", buffer);
+}
+
+bool getLastWeights(int num){
+  /*
+  for(int i = 0; i < num - 1; i++){
+    char c = ' ';
+    while(c != '\n'){
+      c = fgetc(f_o);
+      if(c == EOF)
+        break;
+    }
+  }
+  */
+
+  char *line = malloc(1000);
+  for(int i = 0; i < 1000; i++){
+    line[i] = fgetc(f_o);
+    if(line[i] == '\n' || line[i] == EOF)
+      break;
+  }
+
+  char *token = strtok(line, &delim);
+  if(!token || token[0] == EOF)
+    return false;
+  strtol(token, NULL, 10);
+  token = strtok(NULL, &delim);
+  ai_birdSpeedW = strtof(token, NULL);
+  token = strtok(NULL, &delim);
+  ai_birdHeightW = strtof(token, NULL);
+  token = strtok(NULL, &delim);
+  ai_canvasHeightW = strtof(token, NULL);
+  token = strtok(NULL, &delim);
+  ai_distanceToBarW = strtof(token, NULL);
+  token = strtok(NULL, &delim);
+  ai_barHeightW = strtof(token, NULL);
+}
+
+int main(int argc, char **argv){
+  if(argc < 3){
+    printf("./weightGen [batch] [how many]\n");
+    return -1;
+  }
+
+  time_t t;
+  srand((unsigned) time(&t));
+
+  int batch = atoi(argv[1]);
+  int lastBatch = batch - 1;
+  int num = atoi(argv[2]);
+  float range = 1/(batch);
+
+  int hundreds = batch/100;
+  int tens = (batch - 100*hundreds)/10;
+  filename_i[14] = hundreds + '0';
+  filename_i[15] = tens + '0';
+  filename_i[16] = (batch - 100*hundreds - 10*tens) + '0';
+
+  hundreds = lastBatch/100;
+  tens = (lastBatch - 100*hundreds)/10;
+  filename_o[14] = hundreds + '0';
+  filename_o[15] = tens + '0';
+  filename_o[16] = (lastBatch - 100*hundreds - 10*tens) + '0';
+
+  f_i = fopen(filename_i, "a");
+  printf("%s\n", filename_i);
+  if(f_i == NULL){
+    f_i = fopen(filename_i, "w");
+    if(f_i == NULL){
+      printf("Cannot open the file\n");
+      return -1;
+    }
+    fclose(f_i);
+    f_i = fopen(filename_i, "a");
+  }
+
+  if(batch != 1){
+    f_o = fopen(filename_o, "r");
+    if(!f_o){
+      printf("weightGen.c: Error locating %s.\n", filename_o);
+      return -1;
+    }
+  }
+
+  char *buffer = malloc(100);
+
+  float fluctuation = 1; //0.9 to 0.00001 (last batch)
+  if(batch != 1)
+    fluctuation = 1.0/(3.0*batch) + 1.0/(4.0*batch);
+
+  int mult = pow(batch, 3); //How many new weight sets will be generated using one successful weight set
+
+  for(int i = 0; i < num; i++){
+    ai_birdSpeedW = ai_birdHeightW = ai_canvasHeightW = ai_distanceToBarW = ai_barHeightW = 0;
+    if(batch != 1)
+      getLastWeights(i);
+    for(int j = 0; j < mult; j++){
+      if(rand()%2 == 0)
+        ai_birdSpeedW += (rand()%1000000)/1000000.0*fluctuation;
+      else
+        ai_birdSpeedW -= (rand()%1000000)/1000000.0*fluctuation;
+      if(rand()%2 == 0)
+        ai_birdHeightW += (rand()%1000000)/1000000.0*fluctuation;
+      else
+        ai_birdHeightW -= (rand()%1000000)/1000000.0*fluctuation;
+      if(rand()%2 == 0)
+        ai_canvasHeightW -= (rand()%1000000)/1000000.0*fluctuation;
+      else
+        ai_canvasHeightW += (rand()%1000000)/1000000.0*fluctuation;
+      if(rand()%2 == 0)
+        ai_distanceToBarW += (rand()%1000000)/1000000.0*fluctuation;
+      else
+        ai_distanceToBarW -= (rand()%1000000)/1000000.0*fluctuation;
+      if(rand()%2 == 0)
+        ai_barHeightW += (rand()%1000000)/1000000.0*fluctuation;
+      else
+        ai_barHeightW -= (rand()%1000000)/1000000.0*fluctuation;
+
+      if(ai_birdSpeedW > 1)
+        ai_birdSpeedW = 1;
+      if(ai_birdSpeedW < 0)
+        ai_birdSpeedW = 0;
+
+      if(ai_birdHeightW > 1)
+        ai_birdHeightW = 1;
+      if(ai_birdHeightW < 0)
+        ai_birdHeightW = 0;
+
+      if(ai_canvasHeightW > 1)
+        ai_canvasHeightW = 1;
+      if(ai_canvasHeightW < 0)
+        ai_canvasHeightW = 0;
+
+      if(ai_distanceToBarW > 1)
+        ai_distanceToBarW = 1;
+      if(ai_distanceToBarW < 0)
+        ai_distanceToBarW = 0;
+
+      if(ai_barHeightW > 1)
+        ai_barHeightW = 1;
+      if(ai_barHeightW < 0)
+        ai_barHeightW = 0;
+
+      printWeights(buffer);
+    }
+  }
+  fclose(f_i);
+  if(batch != 1)
+    fclose(f_o);
+  free(buffer);
+}
