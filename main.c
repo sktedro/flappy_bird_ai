203151ad526390f2e02acad7d07f24eb8434706f
Tedro <sktedro@gmail.com>
2021-03-05 11:54:57 +0100

    Now capable of getting weights from ai_getWeights.h and deciding

--- main.c
+++ main.c
@@ -1,6 +1,31 @@
 #include "flappy_bird.h"
+#include "ai_getWeights.h"
 
-int main(){
+#define ai 1
+
+bool ai_jump(){
+  float decision = 
+    ai_birdSpeed * ai_birdSpeedW +
+    ai_birdHeight * ai_birdHeightW +
+    ai_canvasHeight * ai_canvasHeightW +
+    ai_distanceToBar * ai_distanceToBarW +
+    ai_barHeight * ai_barHeightW;
+  if(decision > 0.5 * 5)
+    return true;
+  return false;
+}
+
+int main(int argc, char **argv){
+  int batch, setting;
+  if(ai){
+    if(argc < 3)
+      return -1;
+    ai_canvasHeight = canvasy;
+    batch = atoi(argv[1]);
+    setting = atoi(argv[2]);
+    ai_getWeights(batch, setting);
+  }
+
   char canvas[canvasx][canvasy];
   Bird bird;
   if(!birdInit(&bird))
@@ -18,7 +43,7 @@ int main(){
   bool gameOver = false;
 
   while(1){
-    if(jump())
+    if(ai && jump() || !ai && ai_jump())
       bird.fall_speed = -jumpHeight;
 
     if(!checkBird(bird, &bar, &score))
@@ -34,14 +59,20 @@ int main(){
       gettimeofday(&lastCheck, NULL);
       bird.fall_speed += timediff/4 + 0.3;
       bird.y = bird.y + bird.fall_speed;
+      ai_birdSpeed = bird.fall_speed;
+      ai_birdHeight = bird.y;
 
       //update canvas in memory
       updateBird(&bird);
       updateBar(&bar);
 
+      ai_distanceToBar = (bar.x1 + bar.x2)/2 - birdx;
+
       //print updaged canvas
       printCanvas(canvas, bird, bar);
-      printf("Speed: %g\nY coordinate: %g\nScore = %d\n", bird.fall_speed, bird.y, score);
+      printf("\nScore = %d\n\n", score);
+      printf("Canvas Height: %d\nSpeed: %g\nHeight: %g\nDistance to next bar: %g\nNext bar height: %d\n",
+          ai_canvasHeight, ai_birdSpeed, ai_birdHeight, ai_distanceToBar, ai_barHeight);
     }
   }
   freeBirdPixels(bird);
