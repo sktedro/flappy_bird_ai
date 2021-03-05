2be8b435f83f00e04d3ef144202454e538bb1580
Tedro <sktedro@gmail.com>
2021-03-05 22:55:52 +0100

    Generating and running second batch seems to be working. Test overnight

--- genRunTill.sh
+++ genRunTill.sh
@@ -28,24 +28,34 @@ fi
 out="_o"
 in="_i"
 
-input=$str1$str2$in
-records=$str1$str2$out
+inputPath=$str1$str2$in
+recordsPath=$str1$str2$out
 
 weightGenPath="./weightGen/weightGen"
 
-lines=`wc -l $records`
-lines=$(echo $lines | cut -d' ' -f 1)
+#eval touch $recordsPath
+#eval touch $inputPath
 
-while [ $lines -le $wantedRecords ]; do
-  lines=`wc -l $records`
-  lines=$(echo $lines | cut -d' ' -f 1)
-  eval touch $input
+recordLines=`wc -l $recordsPath`
+recordLines=$(echo $recordLines | cut -d' ' -f 1)
+
+while [ $recordLines -lt $wantedRecords ]; do
+  eval touch $inputPath
   eval $weightGenPath $batch 100
 
-  #eval sh autorun.sh $batch
-  for i in $(seq 1 $lines ); do
+
+  inputLines=`wc -l $inputPath`
+  inputLines=$(echo $inputLines | cut -d' ' -f 1)
+
+  for i in $(seq 1 $inputLines ); do
     eval ./main $batch $i
   done
 
-  eval rm -f $input
+  eval rm -f $inputPath
+
+  recordLines=`wc -l $recordsPath`
+  recordLines=$(echo $recordLines | cut -d' ' -f 1)
+  if [ $recordLines -ge $wantedRecords ]; then
+    exit
+  fi
 done
