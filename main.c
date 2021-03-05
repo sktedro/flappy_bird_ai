a01086df6f35471232f09e4d41c18b55c71e5147
Tedro <sktedro@gmail.com>
2021-03-05 10:45:10 +0100

    Another refactor. Much nicer now

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 #include <math.h>
 
 #define gravity 0.05
-#define speed 100 //default: 100
+#define speed 100 //default: 100 - the higher the slower
 #define jumpHeight 2
 
 #define canvasx 100 //canvas width
@@ -119,20 +119,20 @@ void newBar(Barrier *bar){
   bar->x2 = bar->x2 = canvasx + barWidth;
 }
 
-bool checkBird(Bird bird, Barrier *bar, int score){
+bool checkBird(Bird bird, Barrier *bar, int *score){
   if(bar->x2 < 0){
     newBar(bar);
-    score++;
+    (*score)++;
   }
   if(bird.y < 0 || bird.y > canvasy){
-    printf("You lost. Score: %d\n", score);
+    printf("You lost. Score: %d\n", *score);
     return false;
   }
   for(int i = 0; i < birdPixels; i++){
     for(int j = 0; j < 2; j++){
       if(bird.pixels[i][0] >= bar->x1 && bird.pixels[i][0] <= bar->x2){
         if(bird.pixels[i][1] <= bar->lowy || bird.pixels[i][1] >= bar->highy){
-          printf("You lost. Score: %d\n", score);
+          printf("You lost. Score: %d\n", *score);
           return false;
         }
       }    
@@ -141,7 +141,9 @@ bool checkBird(Bird bird, Barrier *bar, int score){
   return true;
 }
 
-bool birdPixelsInit(Bird *bird){
+bool birdInit(Bird *bird){
+  bird->y = (int)canvasy/2;
+  bird->fall_speed = 0;
   bird->pixels = malloc(birdPixels*sizeof(void*));
   if(!bird->pixels)
     return false;
@@ -160,58 +162,71 @@ bool birdPixelsInit(Bird *bird){
   return true;
 }
 
+void updateBird(Bird *bird){
+  bird->pixels[0][1] = bird->pixels[1][1] = bird->pixels[2][1] = bird->pixels[3][1] = bird->y;
+  bird->pixels[4][1] = bird->pixels[5][1] = bird->pixels[6][1] = bird->pixels[7][1] =
+    bird->pixels[12][1] = bird->y+1;
+  bird->pixels[8][1] = bird->pixels[9][1] = bird->pixels[10][1]= bird->pixels[11][1]=
+    bird->pixels[13][1] = bird->y-1;
+}
+
+void updateBar(Barrier *bar){
+  (bar->x1)--;
+  (bar->x2)--;
+  (bar->x1)--;
+  (bar->x2)--;
+}
+
+void freeBirdPixels(Bird bird){
+  for(int i = 0; i < birdPixels; i++)
+    free(bird.pixels[i]);
+  free(bird.pixels);
+}
+
 int main(){
   char canvas[canvasx][canvasy];
-
   Bird bird;
-  bird.y = (int)canvasy/2;
-  bird.fall_speed = 0;
-  if(!birdPixelsInit(&bird))
+  if(!birdInit(&bird))
     return -1;
-
-  Barrier bar = {0};
+  Barrier bar;
 
   time_t t = 0;
   srand((unsigned) time(&t));
   gettimeofday(&lastCheck, NULL);
+  gettimeofday(&actTime, NULL);
+  uint64_t time = actTime.tv_usec + actTime.tv_sec;
+  uint64_t nextUpdate = 0, timediff = 0;
 
-  bool gameOver = false;
   int score = -1;
+  bool gameOver = false;
 
   while(1){
     if(jump())
       bird.fall_speed = -jumpHeight;
 
-    if(!checkBird(bird, &bar, score))
+    if(!checkBird(bird, &bar, &score))
       break;
 
     gettimeofday(&actTime, NULL);
-    if((double)actTime.tv_usec < (double)lastCheck.tv_usec){
-      lastCheck.tv_usec = 0;
-    }
+    uint64_t time = actTime.tv_usec + actTime.tv_sec*1000000;
+    if(time > nextUpdate){
+      nextUpdate = time + 400*speed;
 
-    if((double)actTime.tv_usec > (double)lastCheck.tv_usec + 400*speed){
-      int timediff = (double)actTime.tv_sec - (double)lastCheck.tv_sec; 
+      //calculate next position of the bird - gravity
+      timediff = (uint64_t)actTime.tv_sec - (uint64_t)lastCheck.tv_sec;
+      gettimeofday(&lastCheck, NULL);
       bird.fall_speed += timediff/4 + 0.3;
       bird.y = bird.y + bird.fall_speed;
 
-      bird.pixels[0][1] = bird.pixels[1][1] = bird.pixels[2][1] = bird.pixels[3][1] = bird.y;
-      bird.pixels[4][1] = bird.pixels[5][1] = bird.pixels[6][1] = bird.pixels[7][1] =
-        bird.pixels[12][1] = bird.y+1;
-      bird.pixels[8][1] = bird.pixels[9][1] = bird.pixels[10][1]= bird.pixels[11][1]=
-        bird.pixels[13][1] = bird.y-1;
+      //update canvas in memory
+      updateBird(&bird);
+      updateBar(&bar);
 
-      gettimeofday(&lastCheck, NULL);
-
-      (bar.x1)--;
-      (bar.x2)--;
-      (bar.x1)--;
-      (bar.x2)--;
-
+      //print updaged canvas
       printCanvas(canvas, bird, bar);
-
       printf("Speed: %g\nY coordinate: %g\nScore = %d\n", bird.fall_speed, bird.y, score);
     }
   }
+  freeBirdPixels(bird);
   return 0;
 }
